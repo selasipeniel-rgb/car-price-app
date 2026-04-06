59f0e08f060d417d2574ef85082f207a8ed0dcaf
selasipeniel-rgb <selasipeniel@gmail.com>
2026-04-06 00:04:39 +0000

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-pip install -r requirements.txt
-pip install jupyter nbconvert
-jupyter nbconvert --to notebook --execute car_prediction_final.ipynb --output car_prediction_final.ipynb
